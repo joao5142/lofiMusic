2d12a94918bb7453133457333cde9a56afc8d76c
MVC-EDITORA-25\MVC EDITORA - 25 <joaopaulofnmvc@gmail.com>
2022-06-10 17:00:35 -0300

    Adicionando evento de clique no progress

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,8 @@ const btnPlay = document.getElementById("btnPlay");
 const btnNext = document.getElementById("btnNext");
 const volumeRange = document.getElementById("volumeRange");
 const btnVolume = document.getElementById("btnVolume");
-
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progressContainer");
 const audioEl = document.getElementById("audio");
 
 let isPlaying = false;
@@ -51,24 +52,31 @@ function prevMusic() {
   isPlaying = false;
   loadMusic(musicas[indexMusic]);
 }
-function setProgress() {}
+
+function setProgress(fullDuration, currentTime) {
+  let percentage = (currentTime / fullDuration) * 100;
+  progress.style.width = `${percentage}%`;
+  //   console.log("POrcentagem : " + percentage);
+}
 
 // Listeners
 btnPlay.addEventListener("click", playPauseMusic);
 
 audioEl.addEventListener("timeupdate", (e) => {
   let currentTime = e.target.currentTime;
+  let fullDuration = e.target.duration;
   let minutes = Math.floor(currentTime / 60);
   let seconds = Math.floor(currentTime % 60);
 
   let timeString = `${"0" + minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
   durationMusic.textContent = timeString;
 
-  setProgress();
+  setProgress(fullDuration, currentTime);
 
-  console.log(audioEl.duration);
+  //   console.log(audioEl.duration);
   console.log(minutes + ":" + seconds);
 });
+audioEl.addEventListener("ended", nextMusic);
 
 btnNext.addEventListener("click", nextMusic);
 btnPrev.addEventListener("click", prevMusic);
@@ -76,6 +84,10 @@ btnPrev.addEventListener("click", prevMusic);
 //volume range
 volumeRange.addEventListener("input", (e) => {
   audioEl.volume = e.target.value / 100;
+});
+//progressContainer
+progressContainer.addEventListener("click", (e) => {
+  console.log(e.offsetX);
 });
 
 loadMusic(musicas[indexMusic]);
